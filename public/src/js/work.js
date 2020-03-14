717e6137e609cd06b5198d0f6254a39b9de0fd0a
plnttr1003 <top@nrdn.ru>
2020-03-14 18:18:53 +0300

    slideshow image

--- public/src/js/work.js
+++ public/src/js/work.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.dot:first').text('●');
-	var count = $('.background_item').length - 1;
+	var initCount = $('.background_item').length - 1;
+	var count = initCount;
 	var w_count = 0;
 
 	function toggleGlow () {
@@ -33,24 +34,35 @@ $(document).ready(function() {
 	}
 
 	function backScroll(event) {
+			var backScroll = ($(window).width() / 2) > event.clientX ? false : true;
 			var target = $(event.target);
+
 			$('.round_click_block').hide();
 			$('.background_works').addClass('glow');
 
 			if (target.is( ':not(.logo, .menu_items a)' )) {
-				w_count++;
+				var currentCount = backScroll ? count - 1 : count + 1;
+
+				currentCount = !backScroll && initCount === count ? 0 : currentCount;
+				w_count = backScroll ? w_count + 1 : w_count - 1;
 				$('.work_item').hide();
 				$('.work_item').eq(w_count).show();
 				$('.dot').text('◦');
 				$('.dot').eq(w_count).text('●');
 				$('.background_item').eq(count).css('opacity', '0');
-				$('.background_item').eq(count-1).css('opacity', '1');
-				count--;
+				$('.background_item').eq(currentCount).css('opacity', '1');
+				count = backScroll ? count - 1 : count + 1;
 
-				if (count < 0) {
-					$('.background_item').css('opacity', '1')
-					count = $('.background_item').length - 1;
+				if (count < 0 && backScroll) {
+					count = initCount;
 					w_count = 0;
+					$('.work_item').eq(w_count).show();
+					$('.dot').eq(w_count).text('●');
+				}
+
+				if (count > initCount && !backScroll) {
+					count = 0;
+					w_count = initCount;
 					$('.work_item').eq(w_count).show();
 					$('.dot').eq(w_count).text('●');
 				}
